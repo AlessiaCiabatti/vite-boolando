b0b73a061cfd7a05a1635f9de7b3eac3d138b7db
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-03-21 15:18:48 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+  export default {
+    
+  }
+</script>
+
+<template>
+  <div>
+    <h1>ciao</h1>
+  </div>
+</template>
+
+<style lang="scss">
+// importo mian.scss
+
+@use './assets/scss/main.scss'
+</style>

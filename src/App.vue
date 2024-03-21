20c29bac4ea37d017ffc29b06fd376b7c2758fc3
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-03-21 16:26:36 +0100

    [add] header-main-footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+
   export default {
-    
+    components:{
+      Header,
+      Main,
+      Footer,
+    }
   }
 </script>
 
+
+
 <template>
-  <div>
-    <h1>ciao</h1>
-  </div>
+  <Header />
+  <Main />
+  <Footer />
 </template>
+
+
 
 <style lang="scss">
 // importo mian.scss
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<script>
+  export default {
+    
+  }
+</script>
+
+<template>
+  <div>
+MAIN
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>

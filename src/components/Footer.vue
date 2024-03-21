f5b58b17c71783b88aa12d34cdc56c5f1931bace
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-03-21 17:16:54 +0100

    [add] main-footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,11 +5,84 @@
 </script>
 
 <template>
-  <div>
-FOOTER
+<div class="container-footer">
+    <div class="text-footer">
+      <div class="boolando">
+        <h4>Boolando s.r.l.</h4>
+        <div class="menu">
+          <ul>
+            <li>
+              <a href="#">Informazioni legali</a>
+            </li>
+            <li>
+              <a href="#">Informativa sulla privacy</a>
+            </li>
+            <li>
+              <a href="#">Diritto di recesso</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="social">
+        <div class="trovaci">
+          <h4>Trovaci anche su</h4>
+          <div class="icone-footer">
+            <i class="fa-brands fa-square-twitter"></i>
+            <i class="fa-brands fa-square-facebook"></i>
+            <i class="fa-brands fa-square-instagram"></i>
+            <i class="fa-brands fa-square-pinterest"></i>
+            <i class="fa-brands fa-square-youtube"></i>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
 
+<style lang="scss">
+.container-footer{
+  background-color: #1a1a1a;
+  height: 80px;
+}
+
+.container-footer .text-footer{
+  max-width: 1030px;
+  height: 80px;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+}
+
+.text-footer .boolando{
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.text-footer .menu a{
+  font-size: 11px;
+}
+
+.text-footer .social{
+  display: flex;
+  align-items: center;
+}
+
+.text-footer .trovaci{
+  color: white;
+  display: flex;
+  flex-direction: column;
+}
+
+.social h4{
+  font-size: 11px;
+  padding-bottom: 6px;
+}
+
+.social .fa-brands{
+  padding-right: 5px;
+}
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,11 +5,16 @@
 </script>
 
 <template>
-  <div>
-MAIN
-  </div>
+<div class="container-main">
+  MAIN
+</div>
 </template>
 
 <style lang="scss">
+@use '../assets/scss/partials/variables' as *;
 
+.container-main{
+  background-color: $bg-main;
+  min-height: calc(100vh - 135px)
+}
 </style>
